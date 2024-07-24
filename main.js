7fbc439874259d5a37b2b1536efb553730c166f4
Giovana439 <116595560+Giovana439@users.noreply.github.com>
2024-07-24 11:42:24 -0300

    g

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
+
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent= tamanhoSenha;
+numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('.parametro-senha__botao')
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentoTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-        //tamanhoSenha = tamanhoSenha-1;
+       // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
-    } 
-    numeroSenha.textContent= tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
-function aumentoTamanho(){
-    if(tamanhoSenha < 20){
-        //tamanhoSenha = tamanhoSenha +1;
-        tamanhoSenha++;
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
