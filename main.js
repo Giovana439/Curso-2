7e8f6a956a2492a572c2e2b951bc77d9a840a15a
Giovana439 <116595560+Giovana439@users.noreply.github.com>
2024-07-24 11:20:50 -0300

    g

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ numeroSenha.textContent= tamanhoSenha;
 const botoes = document.querySelector('.parametro-senha__botao')
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentoTamanho;
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent= tamanhoSenha;
 }
+function aumentoTamanho(){
+    tamanhoSenha = tamanhoSenha +1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+
 console.log(botoes);
