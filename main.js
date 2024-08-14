f0efcabaf57bd0ff67e2926da04ca4de4faaca11
Giovana439 <116595560+Giovana439@users.noreply.github.com>
2024-08-14 11:50:58 -0300

    r

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
@@ -74,5 +73,5 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
